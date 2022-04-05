e148341c605c79ec81ad178fd404472eb8d78257
alok2426 <103025672+alok2426@users.noreply.github.com>
2022-04-05 12:56:02 +0530

    Add files via upload

---
+++ first program.c
@@ -0,0 +1,27 @@
+
+
+//My First Program
+
+#include<stdio.h>
+
+int main()
+
+{
+
+
+   printf("This is my First Program \n");
+ 
+   printf("Hello World\n");
+ 
+   return 0 ;
+
+}
+
+
+
+
+OUTPUT 
+
+
+This is my First Program 
+Hello World
