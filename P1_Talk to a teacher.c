5b9354889e5a9c4f18a6ddc5910a39fd2c6cab90
alok2426 <103025672+alok2426@users.noreply.github.com>
2022-04-26 00:22:39 -0700

    Add files via upload

---
+++ P1_Talk to a teacher.c
@@ -0,0 +1,20 @@
+//My First C program
+
+#include<stdio.h>
+
+int main()
+
+{
+    
+ printf("Talk to a teacher\n");
+   
+ printf("Welcome\n");
+    
+ return 0;
+
+}
+
+
+OUTPUT:
+Talk to a teacher
+Welcome
