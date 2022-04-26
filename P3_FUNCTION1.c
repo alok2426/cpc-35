6ea0d32386310f6851bbcab937bb6e5396d44d10
alok2426 <103025672+alok2426@users.noreply.github.com>
2022-04-26 00:23:09 -0700

    Add files via upload

---
+++ P3_FUNCTION1.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void add()
+{
+    int a=2;
+    int b=3;
+    int c = a+b;
+    printf("Sum of a and b is %d\n",c);
+}
+void main()
+{
+    add();
+}
+OUTPUT:
+Sum of a and b is 5
