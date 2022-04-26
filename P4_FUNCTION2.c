f6eec7ac85652a3c9667efd79009aa14c8e9a942
alok2426 <103025672+alok2426@users.noreply.github.com>
2022-04-26 00:23:39 -0700

    Add files via upload

---
+++ P4_FUNCTION2.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int add(int a,int b)
+{
+    int c= a+ b;
+    
+}
+int main()
+{
+    int sum;
+    sum=add(5,4);
+    printf("Sum is %d\n",sum);
+    return 0;
+
+}
+OUTPUT:
+Sum is 9
