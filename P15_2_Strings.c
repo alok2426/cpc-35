8fb4f1c27735d8e43bba6603101c0b44a0664112
alok2426 <103025672+alok2426@users.noreply.github.com>
2022-06-07 13:13:25 +0530

    Create P15_2_Strings.c

---
+++ P15_2_Strings.c
@@ -0,0 +1,14 @@
+//This program is to show application of String//
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{ 
+    char strname [40]= "Alok Pal";
+    printf ("The string is %s\n", strname);
+    return 0;
+}
+
+
+Output-
+The string is Alok Pal
